80711e2a3271167b80c0d1175031d9d9957f3384
Lukas Joeressen <lukas@joeressen.net>
2014-08-11 16:06:27 +0200

    Improved wav

--- wav.c
+++ wav.c
@@ -1,12 +1,18 @@
 #include "wav.h"
 
+/* Shortcut for for loops in standard form. */
 #define FOR(i, n) for (i = 0; i < n; ++i)
 
-typedef uint8_t u8;
+/* Shortcuts for integer types. */
+typedef  uint8_t u8;
+typedef   int8_t i8;
+typedef uint16_t u16;
+typedef  int16_t i16;
 typedef uint32_t u32;
+typedef  int32_t i32;
 typedef uint64_t u64;
 
-/* Read 32-bit int from buffer. */
+/* Read 32-bit int from x in little endian. */
 static u32 ld_u32_le(const u8 *x)
 {
   u32 u = x[3];
@@ -15,35 +21,38 @@ static u32 ld_u32_le(const u8 *x)
   return (u<<8)|x[0];
 }
 
-/* Read 16-bit int from buffer. */
+/* Read 16-bit int from x in little endian. */
 static u32 ld_u16_le(const u8 *x)
 {
   u32 u = x[1];
   return (u<<8)|x[0];
 }
 
-/* Store 32-bit int to buffer. */
+/* Store 32-bit int to x in little endian. */
 static void st_u32_le(u8 *x, u32 u)
 {
   int i;
   FOR(i, 4) { x[i] = u; u >>= 8; }
 }
 
-/* Store 16-bit int to buffer. */
+/* Store 16-bit int to x in little endian. */
 static void st_u16_le(u8 *x, u32 u)
 {
   int i;
   FOR(i, 2) { x[i] = u; u >>= 8; }
 }
 
-/* Copys l bytes */
+/* Copies l bytes from from to to. */
 static void byte_copy(u8 *to, u64 l, const u8 *from)
 {
   u64 i;
   FOR(i, l) to[i] = from[i];
 }
 
-/* Checks l bytes for equality. */
+/* Checks l bytes for equality.
+ * Caveat: The function returns as soon as it detects a difference and
+ * does not necessarily check all bytes. So it is vulnurable to timing
+ * attacks. */
 static int byte_equal(const u8 *a, u64 l, const u8 *b)
 {
   u64 i;
@@ -69,9 +78,10 @@ static int byte_equal(const u8 *a, u64 l, const u8 *b)
 36 - 40: "data"
 40 - 44: Size of data chunk */
 
-int wav_header_read(wav_header_t *header, uint8_t *buffer)
+int wav_header_read(wav_header_t *header, const u8 *buffer)
 {
   u32 num_channels, frame_size, bit_depth, sample_rate, file_size, data_size;
+  /* Check for valid pointers. */
   if (!header || !buffer) return -1;
   num_channels = ld_u16_le(buffer + 22);
   frame_size = ld_u16_le(buffer + 32);
@@ -85,12 +95,13 @@ int wav_header_read(wav_header_t *header, uint8_t *buffer)
     !byte_equal(buffer +  8, 4, (u8*)"WAVE") ||
     !byte_equal(buffer + 12, 4, (u8*)"fmt ") ||
     !byte_equal(buffer + 36, 4, (u8*)"data") ||
+    /* Format header is always 16 bytes. */
     ld_u32_le(buffer + 16) != 16 ||
-    /* Frame size is redundant. It can be calculated from bit_depth and num_channels. */
+    /* frame_size is redundant. It can be calculated from bit_depth and num_channels. */
     frame_size != num_channels * ((bit_depth + 7) / 8) ||
     /* File must be large enough to contain all the data. */
     data_size + 36 > file_size ||
-    /* Byterate is redundant. It can be calculated from frame_size and sample_rate. */
+    /* byte_rate is redundant. It can be calculated from frame_size and sample_rate. */
     ld_u32_le(buffer + 28) != frame_size * sample_rate
   ) return -1;
   header->format = ld_u16_le(buffer + 20);
@@ -101,12 +112,14 @@ int wav_header_read(wav_header_t *header, uint8_t *buffer)
   return 44;
 }
 
-int wav_header_write(uint8_t *buffer, wav_header_t *header)
+int wav_header_write(u8 *buffer, wav_header_t *header)
 {
   u32 frame_size, data_size;
+  /* Check for valid pointers. */
   if (!header || !buffer) return -1;
   frame_size = header->num_channels * ((header->bit_depth + 7) / 8);
   data_size = header->num_frames * frame_size;
+  /* Fill in the fields. */
   byte_copy(buffer,      4, (u8*)"RIFF");
   st_u32_le(buffer +  4, data_size + 36);
   byte_copy(buffer +  8, 4, (u8*)"WAVE");
@@ -123,27 +136,53 @@ int wav_header_write(uint8_t *buffer, wav_header_t *header)
   return 44;
 }
 
+/* Return frame config. It's contents are:
+ * 2 byte frame_size,
+ * 2 byte format,
+ * 2 byte bit_depth,
+ * 2 byte num_channels. */
 wav_frame_config_t wav_get_frame_config(wav_header_t *header)
 {
   wav_frame_config_t fc = 0;
-  fc |= (header->num_channels & 255);
-  fc |= (header->bit_depth & 255) << 8;
-  fc |= (header->format & 255) << 16;
-  fc |= ((header->num_channels * ((header->bit_depth + 7) / 8)) & 255) << 24;
+  /* Check for valid pointer. */
+  if (!header) return 0;
+  /* Fill in bytes. */
+  fc |= (wav_frame_config_t)(header->num_channels & 0xffff);
+  fc |= (wav_frame_config_t)(header->bit_depth & 0xffff) << 16;
+  fc |= (wav_frame_config_t)(header->format & 0xffff) << 32;
+  fc |= (wav_frame_config_t)((header->num_channels * ((header->bit_depth + 7) / 8)) & 0xffff) << 48;
   return fc;
 }
 
-#define NUM_CHANNELS(fc) ((fc) & 255)
-#define BIT_DEPTH(fc) (((fc) >> 8) & 255)
-#define FORMAT(fc) (((fc) >> 16) & 255)
-#define FRAME_SIZE(fc) (((fc) >> 24) & 255)
+/* Macros for accessing the fields of frame configs. */
+#define NUM_CHANNELS(fc) ((fc) & 0xffff)
+#define BIT_DEPTH(fc) (((fc) >> 16) & 0xffff)
+#define FORMAT(fc) (((fc) >> 32) & 0xffff)
+#define FRAME_SIZE(fc) (((fc) >> 48) & 0xffff)
 
 int wav_get_frame_size(wav_frame_config_t fc)
 {
   return FRAME_SIZE(fc);
 }
 
-int wav_read_int_frame(wav_frame_config_t fc, int32_t *samples, const uint8_t *buffer)
+int wav_get_num_channels(wav_frame_config_t fc)
+{
+  return NUM_CHANNELS(fc);
+}
+
+static i32 read_sample_24(const u8 *b)
+{
+  return (i32)b[0] << 8 | (i32)b[1] << 16 | (i32)b[2] << 24;
+}
+
+static void write_sample_24(u8 *b, i32 s)
+{
+  b[0] = s >> 8;
+  b[1] = s >> 16;
+  b[2] = s >> 24;
+}
+
+int wav_read_int_frame(wav_frame_config_t fc, i32 *samples, const u8 *buffer)
 {
   int i, b;
   if (!samples || !buffer) return -1;
@@ -151,15 +190,19 @@ int wav_read_int_frame(wav_frame_config_t fc, int32_t *samples, const uint8_t *b
   FOR(i, NUM_CHANNELS(fc)) {
     switch (b) {
       case 8:
-        samples[i] = (int32_t)buffer[i] << 24;
+        samples[i] = (i32)buffer[i] << 24;
         break;
       case 12:
       case 16:
-        samples[i] = (int32_t)ld_u16_le(buffer) << 16;
+        samples[i] = (i32)ld_u16_le(buffer) << 16;
         buffer += 2;
         break;
+      case 24:
+        samples[i] = read_sample_24(buffer);
+        buffer += 3;
+        break;
       case 32:
-        samples[i] = (int32_t)ld_u32_le(buffer);
+        samples[i] = (i32)ld_u32_le(buffer);
         buffer += 4;
         break;
       default:
@@ -169,7 +212,7 @@ int wav_read_int_frame(wav_frame_config_t fc, int32_t *samples, const uint8_t *b
   return FRAME_SIZE(fc);
 }
 
-int wav_write_int_frame(wav_frame_config_t fc, uint8_t *buffer, const int32_t *samples)
+int wav_write_int_frame(wav_frame_config_t fc, u8 *buffer, const i32 *samples)
 {
   int i, b;
   if (!samples || !buffer) return -1;
@@ -177,15 +220,19 @@ int wav_write_int_frame(wav_frame_config_t fc, uint8_t *buffer, const int32_t *s
   FOR(i, NUM_CHANNELS(fc)) {
     switch (b) {
       case 8:
-        buffer[i] = (uint8_t)(samples[i] >> 24);
+        buffer[i] = (u8)(samples[i] >> 24);
         break;
       case 12:
       case 16:
-        st_u16_le(buffer, (uint32_t)(samples[i] >> 16));
+        st_u16_le(buffer, (u32)(samples[i] >> 16));
         buffer += 2;
         break;
+      case 24:
+        write_sample_24(buffer, samples[i]);
+        buffer += 3;
+        break;
       case 32:
-        st_u32_le(buffer, (uint32_t)samples[i]);
+        st_u32_le(buffer, (u32)samples[i]);
         buffer += 4;
         break;
       default:
@@ -195,7 +242,7 @@ int wav_write_int_frame(wav_frame_config_t fc, uint8_t *buffer, const int32_t *s
   return FRAME_SIZE(fc);
 }
 
-int wav_read_double_frame(wav_frame_config_t fc, double *samples, const uint8_t *buffer)
+int wav_read_double_frame(wav_frame_config_t fc, double *samples, const u8 *buffer)
 {
   int i, b;
   double s = 0;
@@ -205,20 +252,25 @@ int wav_read_double_frame(wav_frame_config_t fc, double *samples, const uint8_t 
     case 8:  s = 1.0 / 0x7f; break;
     case 12: s = 1.0 / 0x7ff0; break;
     case 16: s = 1.0 / 0x7fff; break;
+    case 24: s = 1.0 / 0x7fffff00; break;
     case 32: s = 1.0 / 0x7fffffff; break;
   }
   FOR(i, NUM_CHANNELS(fc)) {
     switch (b) {
       case 8:
-        samples[i] = (int8_t)buffer[i] * s;
+        samples[i] = (i8)buffer[i] * s;
         break;
       case 12:
       case 16:
-        samples[i] = (int16_t)ld_u16_le(buffer) * s;
+        samples[i] = (i16)ld_u16_le(buffer) * s;
         buffer += 2;
         break;
+      case 24:
+        samples[i] = read_sample_24(buffer) * s;
+        buffer += 3;
+        break;
       case 32:
-        samples[i] = (int32_t)ld_u32_le(buffer) * s;
+        samples[i] = (i32)ld_u32_le(buffer) * s;
         buffer += 4;
         break;
       default:
@@ -228,7 +280,7 @@ int wav_read_double_frame(wav_frame_config_t fc, double *samples, const uint8_t 
   return FRAME_SIZE(fc);
 }
 
-int wav_write_double_frame(wav_frame_config_t fc, uint8_t *buffer, const double *samples)
+int wav_write_double_frame(wav_frame_config_t fc, u8 *buffer, const double *samples)
 {
   int i, b;
   double s = 0;
@@ -238,20 +290,25 @@ int wav_write_double_frame(wav_frame_config_t fc, uint8_t *buffer, const double 
     case 8:  s = 0x7f; break;
     case 12: s = 0x7ff0; break;
     case 16: s = 0x7fff; break;
+    case 24: s = 0x7fffff00; break;
     case 32: s = 0x7fffffff; break;
   }
   FOR(i, NUM_CHANNELS(fc)) {
     switch (b) {
       case 8:
-        buffer[i] = (int8_t)(samples[i] * s);
+        buffer[i] = (i8)(samples[i] * s);
         break;
       case 12:
       case 16:
-        st_u16_le(buffer, (int32_t)(samples[i] * s));
+        st_u16_le(buffer, (i32)(samples[i] * s));
         buffer += 2;
         break;
+      case 24:
+        write_sample_24(buffer, samples[i] * s);
+        buffer += 3;
+        break;
       case 32:
-        st_u32_le(buffer, (int32_t)(samples[i] * s));
+        st_u32_le(buffer, (i32)(samples[i] * s));
         buffer += 4;
         break;
       default:
--- wav.h
+++ wav.h
@@ -3,37 +3,88 @@
 
 #include <stdint.h>
 
+/* Supported formats.
+ * PCM is the only supported format at the moment. */
 #define WAV_PCM 1
 
+/* Structure for storing the WAV header. */
+typedef struct wav_header_s wav_header_t;
 struct wav_header_s {
+  /* Sample format. Must be one of the supported formats above. */
   uint16_t format;
+  /* Number of channels.
+   * 1 = Mono
+   * 2 = Stereo
+   * More channels are possible (e.g. for surround sound). */
   uint16_t num_channels;
+  /* Sample rate. Common choices for audio are 44100 or 48000. */
   uint32_t sample_rate;
+  /* Sample bit depth. Must be 8, 12, 16, 24 or 32. */
   uint16_t bit_depth;
+  /* Number of frames in the data stream. */
   uint32_t num_frames;
 };
-typedef struct wav_header_s wav_header_t;
 
+/* The required size for a WAV header.
+ * Use this constant to allocate space for the buffer required by
+ * wav_header_read and wav_header_write. */
 #define WAV_HEADER_BYTES 44
 
-/* Reads a wav_header_t from a 44 byte uint8_t buffer.
-   Returns -1 if the buffer is malformed. */
-extern int wav_header_read(wav_header_t *header, uint8_t *buffer);
+/* Reads a wav_header_t from a WAV_HEADER_BYTES byte uint8_t buffer.
+ * If everything went good, it returns the number of bytes read from buffer.
+ * Returns -1 if the buffer is malformed. */
+extern int wav_header_read(wav_header_t *header, const uint8_t *buffer);
 
-/* Writes a wav_header_t to a 44 byte uint8_t buffer. */
+/* Writes a wav_header_t to a WAV_HEADER_BYTES byte uint8_t buffer.
+ * Returns the number of bytes written to buffer.
+ * Returns -1 in case of an error. */
 extern int wav_header_write(uint8_t *buffer, wav_header_t *header);
 
-typedef uint32_t wav_frame_config_t;
+/* Lightweight datatype for reading and writing frames from a data stream.
+ * Due to it's size it can be passed by value. */
+typedef uint64_t wav_frame_config_t;
 
+/* Returns a frame config that is used by the methods for reading and
+ * writing signal frames.
+ * header must be a initialised wav_header_t.
+ * At least format, num_channels and bit_depth must be filled in. */
 extern wav_frame_config_t wav_get_frame_config(wav_header_t *header);
 
-/* Returns the frame size for the given frame config. */
+/* Returns the frame size for the given frame config, i.e., the number
+ * of bytes needed for each frame.
+ * fc must be a wav_frame_config_t as returned form wav_get_frame_config(). */
 extern int wav_get_frame_size(wav_frame_config_t fc);
 
+/* Returns the number of channels for the given frame config.
+ * fc must be a wav_frame_config_t as returned form wav_get_frame_config(). */
+extern int wav_get_num_channels(wav_frame_config_t fc);
+
+/* Reads a signal frame with the specified frame config from buffer[0], buffer[1], ...
+ * buffer[wav_get_frame_size(fc) - 1] and stores it in samples[0], samples[1], ...
+ * samples[wav_get_num_channels(fc) - 1].
+ * It converts the samples to int32_t as appropiate for the frame config.
+ * Returns the number of bytes read from buffer or, in case of an error, -1. */
 extern int wav_read_int_frame(wav_frame_config_t fc, int32_t *samples, const uint8_t *buffer);
+
+/* Writes a signal frame with the specified frame config to buffer[0], buffer[1], ...
+ * buffer[wav_get_frame_size(fc) - 1]. The samples are taken from samples[0], samples[1], ...
+ * samples[wav_get_num_channels(fc) - 1].
+ * It converts the samples from int32_t to the appropiate format for the given frame config.
+ * Returns the number of bytes read from buffer or, in case of an error, -1. */
 extern int wav_write_int_frame(wav_frame_config_t fc, uint8_t *buffer, const int32_t *samples);
 
+/* Reads a signal frame with the specified frame config from buffer[0], buffer[1], ...
+ * buffer[wav_get_frame_size(fc) - 1] and stores it in samples[0], samples[1], ...
+ * samples[wav_get_num_channels(fc) - 1].
+ * It converts the samples to double as appropiate for the frame config.
+ * Returns the number of bytes read from buffer or, in case of an error, -1. */
 extern int wav_read_double_frame(wav_frame_config_t fc, double *samples, const uint8_t *buffer);
+
+/* Writes a signal frame with the specified frame config to buffer[0], buffer[1], ...
+ * buffer[wav_get_frame_size(fc) - 1]. The samples are taken from samples[0], samples[1], ...
+ * samples[wav_get_num_channels(fc) - 1].
+ * It converts the samples from double to the appropiate format for the given frame config.
+ * Returns the number of bytes read from buffer or, in case of an error, -1. */
 extern int wav_write_double_frame(wav_frame_config_t fc, uint8_t *buffer, const double *samples);
 
 #endif
