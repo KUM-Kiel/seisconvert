7696a9a182e1d5f9d66b3e5a123c0e44f5f2d510
Lukas Joeressen <lukas@joeressen.net>
2014-08-21 10:45:32 +0200

    Comments

--- src/seed/miniseed_file.h
+++ src/seed/miniseed_file.h
@@ -32,20 +32,41 @@ struct miniseed_file_s {
   seed_blockette_1000_t blockette_1000;
   /* Write buffer for next record. */
   uint8_t *write_buffer;
-  struct taia sample_interval;
   uint64_t write_pos;
+  struct taia sample_interval;
 };
 
+/* Open a MiniSEED file for reading.
+ * Supports only big endian files.
+ * Supports 32bit sample format and STEIM(1) compression. */
 extern miniseed_file_t *miniseed_file_open(const char *path);
+
+/* Creates a MiniSEED file for writing.
+ * Before writing frames to the file you MUST call
+ * miniseed_file_set_start_time and miniseed_file_set_sample_rate. */
 extern miniseed_file_t *miniseed_file_create(const char *path);
+
+/* Closes an open MiniSEED file, frees it's resources and flushes all frames to
+ * disk. */
 extern void miniseed_file_close(miniseed_file_t *file);
 
+/* Sets the start time of the recording. Must be called exactly once at the
+ * beginning on files in write mode. */
 extern int miniseed_file_set_start_time(miniseed_file_t *file, struct taia *t);
+
+/* Sets the sample rate of the recording. Must be called exactly once at the
+ * beginning on files in write mode. */
 extern int miniseed_file_set_sample_rate(miniseed_file_t *file, uint32_t sample_rate);
 
+/* Reads an int frame from a MiniSEED file in read mode.
+ * Returns -1 if there is no more data in the file.
+ * Returns -3 if there was an error or the file is malformed. */
 extern int miniseed_file_read_int_frame(miniseed_file_t *file, int32_t *frame);
+/* Writes an int frame to a MiniSEED file in write mode.
+ * Returns -3 in case of an error. */
 extern int miniseed_file_write_int_frame(miniseed_file_t *file, const int32_t *frame);
 
+/* Exactly like the int functions, but with doubles. */
 extern int miniseed_file_read_double_frame(miniseed_file_t *file, double *frame);
 extern int miniseed_file_write_double_frame(miniseed_file_t *file, const double *frame);
 
--- src/wav/wav_file.h
+++ src/wav/wav_file.h
@@ -25,13 +25,29 @@ struct wav_file_s {
   uint8_t *buffer;
 };
 
+/* Opens a WAV file for reading. */
 extern wav_file_t *wav_file_open(const char *path);
+
+/* Creates a WAV file for writing with the given sample rate, number of channels
+ * and bit depth. */
 extern wav_file_t *wav_file_create(const char *path, unsigned long sample_rate, unsigned int num_channels, unsigned int bit_depth);
+
+/* Closes an open WAV file, frees it's resources and writes all pending frames
+ * to disk. */
 extern void wav_file_close(wav_file_t *file);
 
+/* Reads a frame from the WAV file and stores it as 32 bit integers.
+ * The supplied frame pointer must have enough space for one int32_t per channel.
+ * Returns -1 if there is no more data in the file.
+ * Returns -3 if there was an error or the file is malformed. */
 extern int wav_file_read_int_frame(wav_file_t *file, int32_t *frame);
+
+/* Writes a frame to the WAV file. The values are taken from the supplied frame
+ * pointer, one int32_t per channel. */
 extern int wav_file_write_int_frame(wav_file_t *file, const int32_t *frame);
 
+/* Exactly like the int functions, but with doubles.
+ * The values range from -1.0 to 1.0. */
 extern int wav_file_read_double_frame(wav_file_t *file, double *frame);
 extern int wav_file_write_double_frame(wav_file_t *file, const double *frame);
 
